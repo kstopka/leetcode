cbc7247bdc1e1ccba3a34f81c9ae8f0d62b21190
Krystian Stopka <kr6stopka@gmail.com>
2025-07-02 11:34:57 +0200

    607_Sales_Person

--- 35_Search_Insert_Position.py
+++ 35_Search_Insert_Position.py
@@ -6,16 +6,20 @@ from typing import List
 
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        for idx, x in enumerate(nums):
-            if x == target:
-                return idx
-            elif x < target and idx + 1 == len(nums):
-                return len(nums)
-            elif x < target and nums[idx + 1] > target:
-                return idx + 1
+        # for idx, x in enumerate(nums):
+        #     if x == target:
+        #         return idx
+        #     elif x < target and idx + 1 == len(nums):
+        #         return len(nums)
+        #     elif x < target and nums[idx + 1] > target:
+        #         return idx + 1
 
-        return 0
-    
+        # return 0
+        
+        for idx, x in enumerate(nums):
+            if(x >= target): return idx
+        return len(nums)
+
 sol = Solution()
 print(sol.searchInsert([1,3,5,6], 5))
 print(sol.searchInsert([1,3,5,6], 2))
