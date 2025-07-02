2cc3bc7366a0c68ce5afc2a9a332864d41fce980
Krystian Stopka <kr6stopka@gmail.com>
2025-07-02 09:05:40 +0200

    35_Search_Insert_Position

---
+++ 35_Search_Insert_Position.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/search-insert-position/description/
+# Easy
+# Array
+# Binary Search
+from typing import List
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        for idx, x in enumerate(nums):
+            if x == target:
+                return idx
+            elif x < target and idx + 1 == len(nums):
+                return len(nums)
+            elif x < target and nums[idx + 1] > target:
+                return idx + 1
+
+        return 0
+    
+sol = Solution()
+print(sol.searchInsert([1,3,5,6], 5))
+print(sol.searchInsert([1,3,5,6], 2))
+print(sol.searchInsert([1,3,5,6], 7))
