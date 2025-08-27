2b06ef380ed50c941d21e3b36c5d9dc48fbc1485
Krystian Stopka <kr6stopka@gmail.com>
2025-08-27 11:52:55 +0200

    2723. Add Two Promises.js

---
+++ 2723. Add Two Promises.js
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/add-two-promises/description/
+
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function (promise1, promise2) {
+  // const first = await promise1;
+  // const second = await promise2;
+  return (await promise1) + (await promise2);
+};
+
+addTwoPromises(Promise.resolve(2), Promise.resolve(2)).then(console.log); // 4
