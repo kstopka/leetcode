a04fda340dbb88ba2cb1588abb50c69d91d2fa9a
Krystian Stopka <kr6stopka@gmail.com>
2025-09-02 14:37:54 +0200

    2726. Calculator with Method Chaining

---
+++ 2726. Calculator with Method Chaining.js
@@ -0,0 +1,63 @@
+// https://leetcode.com/problems/calculator-with-method-chaining/description/
+
+class Calculator {
+  /**
+   * @param {number} value
+   */
+  constructor(value) {
+    this.value = value;
+  }
+
+  /**
+   * @param {number} value
+   * @return {Calculator}
+   */
+  add(value) {
+    return new Calculator(this.value + value);
+  }
+
+  /**
+   * @param {number} value
+   * @return {Calculator}
+   */
+  subtract(value) {
+    return new Calculator(this.value - value);
+  }
+
+  /**
+   * @param {number} value
+   * @return {Calculator}
+   */
+  multiply(value) {
+    return new Calculator(this.value * value);
+  }
+
+  /**
+   * @param {number} value
+   * @return {Calculator}
+   */
+  divide(value) {
+    if (value === 0) throw new Error("Division by zero is not allowed");
+    return new Calculator(this.value / value);
+  }
+
+  /**
+   * @param {number} value
+   * @return {Calculator}
+   */
+  power(value) {
+    return new Calculator(this.value ** value);
+  }
+
+  /**
+   * @return {number}
+   */
+  getResult() {
+    return this.value;
+  }
+}
+
+const cal = new Calculator(10).add(5).subtract(7).getResult();
+console.log("cal", cal);
+console.log(new Calculator(2).multiply(5).power(2).getResult());
+// console.log(new Calculator(20).divide(0).getResult());
